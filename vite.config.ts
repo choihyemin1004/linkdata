1f52bdb86bd05a9d4153f7d9a505f0f125e3d58a
unknown <choihyemin94@gmail.com>
2025-04-21 09:50:42 +0900

    firstcommit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 이 줄 추가: 루트 도메인에 배포할 경우
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
